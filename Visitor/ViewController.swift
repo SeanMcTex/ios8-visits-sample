e8885abdf14ec0e74f4fad04464154361386c215
Sean McMains <sean.mcmains@mutualmobile.com>
2014-08-21 08:30:15 -0500

    Initial Commit

---
+++ Visitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Visitor
+//
+//  Created by Sean McMains on 8/21/14.
+//  Copyright (c) 2014 Mutual Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
