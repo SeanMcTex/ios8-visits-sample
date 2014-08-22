5f118c0e6acfbeb2710f4440e06ee03f5104b011
Sean McMains <sean.mcmains@mutualmobile.com>
2014-08-22 16:10:22 -0500

    Populating table with core data info.

--- Visitor/ViewController.swift
+++ Visitor/ViewController.swift
@@ -7,18 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate {
+    
+    let entityName = "Visit"
+
+    @IBOutlet var tableView: UITableView!
+    
+    var visitsArray:[Visit] = []
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.fetchVisitsFromStore()
+    }
+    
+    func fetchVisitsFromStore() {
+        let app = UIApplication.sharedApplication().delegate as AppDelegate
+        if let context = app.managedObjectContext {
+            
+            let fetchRequest = NSFetchRequest()
+            let entityDescription = NSEntityDescription.entityForName(entityName, inManagedObjectContext: context)
+            fetchRequest.entity = entityDescription
+            var error :NSError?
+            self.visitsArray = context.executeFetchRequest(fetchRequest, error: &error) as [Visit]
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // MARK: Table View Delegate Methods
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.visitsArray.count 
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("VisitCellIdentifier", forIndexPath: indexPath) as UITableViewCell
+        
+        let visit = self.visitsArray[ indexPath.row ]
+        cell.textLabel.text = "Visit!"
+        cell.detailTextLabel.text = visit.arrivalDate.description
+        
+        return cell
+    }
+
+    
 
 
 }
